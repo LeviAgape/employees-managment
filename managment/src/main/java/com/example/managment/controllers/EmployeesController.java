a26680a00f582a36df3d3bec0ce8918bd2fe1e8b
Levi <agapelevi99@gmail.com>
2024-06-12 20:26:29 -0300

    refactor: Add employee repository and Controller

--- managment/src/main/java/com/example/managment/controllers/EmployeesController.java
+++ managment/src/main/java/com/example/managment/controllers/EmployeesController.java
@@ -14,6 +14,6 @@ public class EmployeesController {
     private EmployeeRepository repository;
     @GetMapping
     public ResponseEntity getAllEmployees(){
-        return  ResponseEntity.ok("deu okk");
+        return  ResponseEntity.ok("deu okkk");
     }
 }
