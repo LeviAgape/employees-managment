00d595b81b759eb38dd7db7747b54ce1b3ae9438
Levi <agapelevi99@gmail.com>
2024-06-12 20:16:40 -0300

    feat: Add employee repository and Controller

--- managment/src/main/java/com/example/managment/controllers/EmployeesController.java
+++ managment/src/main/java/com/example/managment/controllers/EmployeesController.java
@@ -1,5 +1,7 @@
 package com.example.managment.controllers;
 
+import com.example.managment.domain.employee.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/employees")
 public class EmployeesController {
+    @Autowired
+    private EmployeeRepository repository;
     @GetMapping
     public ResponseEntity getAllEmployees(){
         return  ResponseEntity.ok("deu ok");
