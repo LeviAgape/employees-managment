fe2384e939cd8de96a40b72bd010fe72e38735f2
Levi <agapelevi99@gmail.com>
2024-06-12 21:00:37 -0300

    feat: Constructor Employee and add htpp post employee

--- managment/src/main/java/com/example/managment/controllers/EmployeesController.java
+++ managment/src/main/java/com/example/managment/controllers/EmployeesController.java
@@ -1,11 +1,11 @@
 package com.example.managment.controllers;
 
+import com.example.managment.domain.employee.Employee;
 import com.example.managment.domain.employee.EmployeeRepository;
+import com.example.managment.domain.employee.RequestEmployee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/employees")
@@ -14,6 +14,14 @@ public class EmployeesController {
     private EmployeeRepository repository;
     @GetMapping
     public ResponseEntity getAllEmployees(){
-        return  ResponseEntity.ok("deu ok");
+        var allEmployees = repository.findAll();
+        return  ResponseEntity.ok(allEmployees);
+    }
+
+    @PostMapping
+    public ResponseEntity registerEmployee(@RequestBody RequestEmployee data){
+        Employee newEmployee= new Employee(data);
+        repository.save(newEmployee);
+        return ResponseEntity.ok().build();
     }
 }
