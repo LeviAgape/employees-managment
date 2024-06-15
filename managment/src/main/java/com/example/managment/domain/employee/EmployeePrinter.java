57e9ab062723d42c00b2137e724d6f6598dafd86
Levi <agapelevi99@gmail.com>
2024-06-15 11:32:36 -0300

    feat: Apply increase salary

--- managment/src/main/java/com/example/managment/domain/employee/EmployeePrinter.java
+++ managment/src/main/java/com/example/managment/domain/employee/EmployeePrinter.java
@@ -12,29 +12,27 @@ import java.util.Map;
 public class EmployeePrinter {
 
     public static String getEmployeesGroupedByFuncaoAsJson(List<Object[]> employeesGroupedByFuncao) {
-        // Map para agrupar os funcionários por função
+
         Map<String, List<Employee>> groupedEmployees = new HashMap<>();
 
-        // Itera sobre os resultados da consulta
         for (Object[] result : employeesGroupedByFuncao) {
-            String funcao = (String) result[0];  // Obtém a função
-            Employee employee = (Employee) result[1];  // Obtém o funcionário
+            String funcao = (String) result[0];
+            Employee employee = (Employee) result[1];
 
-            // Verifica se a função já está no mapa
             if (!groupedEmployees.containsKey(funcao)) {
                 groupedEmployees.put(funcao, new ArrayList<>());
             }
 
-            // Adiciona o funcionário à lista da função correspondente
+
             groupedEmployees.get(funcao).add(employee);
         }
 
-        // Converter para JSON usando Jackson ObjectMapper
+
         ObjectMapper mapper = new ObjectMapper();
         try {
             return mapper.writeValueAsString(groupedEmployees);
         } catch (JsonProcessingException e) {
-            e.printStackTrace();  // Ou tratar exceção conforme necessário
+            e.printStackTrace();
             return null;
         }
     }
